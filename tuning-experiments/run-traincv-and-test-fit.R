c85d56d54f2a9850b758c538f7f4558e7c97f6d7
Andreas Beger <adbeger@gmail.com>
2020-05-04 12:34:38 +0300

    revamp triancv/test model runner script
    To make it easier to run all models for the base specification rows in Table 1

--- tuning-experiments/run-traincv-and-test-fit.R
+++ tuning-experiments/run-traincv-and-test-fit.R
@@ -6,6 +6,9 @@
 #   information in Table 1.
 #
 
+RERUN   <- TRUE
+WORKERS <- 7
+
 library(tidyverse)
 library(tidymodels)
 library(randomForest)
@@ -18,11 +21,10 @@ library(boot)
 setwd(here::here("tuning-experiments"))
 
 registerDoFuture()
-plan(multisession(workers = 7))
+plan(multisession(workers = WORKERS))
 
-#
-#   Set up model function ----
-#   _______________________
+
+# Set up RF model functions -----------------------------------------------
 #
 #   I'm going to define a couple of model functions here. In Table 1 in the
 #   paper, the results are identified by 3 dimensions:
@@ -134,6 +136,7 @@ predict.rf_tuned <- function(object, new_data, ...) {
 }
 
 
+# Prepare data ------------------------------------------------------------
 
 df <- read_rds("trafo-data/1mo_data.rds") %>%
   mutate(incidence_civil_ns_plus1 = factor(incidence_civil_ns_plus1, levels = c("1", "0")))
@@ -164,12 +167,20 @@ quad <- c(
   "opp_gov_matcp","reb_gov_matcp"
 )
 
-all_cameo <- c(
+goldstein <- c("")
+
+cameo <- c(
   names(df)[str_detect(names(df), "cameo_[0-9]+$")]
 )
 
+# Map the Table 1 columns names to specifications
+spec_list <- list(Escalation = escalation,
+                  Quad = quad,
+                  Goldstein = goldstein,
+                  CAMEO = cameo)
+
 # Make sure we are operating on same df for both specs
-df <- df[complete.cases(df[, unique(c(dv, escalation, all_cameo))]), ]
+df <- df[complete.cases(df[, unique(c(dv, escalation, cameo))]), ]
 
 # Define training and testing sets for base specification
 train_period = mean(df$period[which(df$month==12 & df$year==2007)])
@@ -179,262 +190,165 @@ train_df <- df[df$period<=train_period,]
 test_df  <- df[df$period>train_period & df$period<=end_period,]
 
 
+
+# Set up model grid -------------------------------------------------------
 #
-#   Training data CV ----
-#   _________________
+
+table1_rows <- c("Base specification", "Terminal nodes", "Sample size",
+                 "Trees per forest", "Training/test sets 1",
+                 "Training/test sets 2", "Training/test sets 3",
+                 "Coding of DV 1", "Coding of DV 2")
+table1_rows <- factor(table1_rows, levels = table1_rows)
+horizon <- c("1 month", "6 months")
+horizon <- factor(horizon, levels = horizon)
+table1_columns <- c("Escalation", "Quad", "Goldstein", "CAMEO", "Average")
+table1_columns <- factor(table1_columns, levels = table1_columns)
+
+# This defines all cells in Table 1
+table1 <- crossing(table1_rows, table1_columns, horizon)
+
+# For each cell in table 1, I want to run three versions of RF
+hp_set <- c("B&S", "Default", "Tuned")
+hp_set <- factor(hp_set, levels = hp_set)
+
+# Setup up training CV train/test data indices
+set.seed(1234)
+folds <- vfold_cv(train_df, v = 2, repeats = 7*3) %>%
+  # keep only the row indices for the CV train/validation splits
+  mutate(train_idx = map(splits, function(x) x$in_id),
+         test_idx  = map(splits, function(x) (1:nrow(x$data))[-x$in_id]),
+         splits = NULL)
+# Make sure all splits have at least one positive case, otherwise ROC doesn't
+# work
+pos <- map_dbl(1:nrow(folds), df = train_df, function(i, df) {
+  test_idx <- folds[i, ][["test_idx"]][[1]]
+  sum(train_df[test_idx, ][["incidence_civil_ns_plus1"]]=="1")
+})
+stopifnot(all(pos > 0))
+
+# Add a row to folds for the full models that will use all train_df data and
+# will predict over the test_df data
+test_row <- tibble(
+  prediction_on = "test",
+  id = NA_character_,
+  id2 = NA_character_,
+  train_idx = list(1:nrow(train_df)),
+  test_idx  = list((nrow(train_df) + 1):nrow(df))
+)
+folds <- folds %>%
+  mutate(prediction_on = "train-cv") %>%
+  select(prediction_on, everything()) %>%
+  bind_rows(test_row) %>%
+  # convert prediction to factor otherwise test will show up before train-cv
+  mutate(prediction_on = factor(prediction_on, levels = c("train-cv", "test")))
+
+# Create the full model grid:
+# [all cells in Table 1] x [the 3 RF HP sets] x [the train CV and test data indices]
+full_model_grid <- crossing(table1, hp_set, folds) %>%
+  # create a unique ID for each model
+  mutate(model_id = 1:n()) %>%
+  select(model_id, everything())
+
+# Write a copy of the full grid, sans the actual indices, to a CSV file. This
+# is so that any permutations that alter what rows are associated with what
+# model_id will show up in git. They shouldn't, since a permutation of model IDs
+# would be a fatal mistake that breaks the link between model and correct chunk
+# file.
+full_model_grid %>%
+  select(model_id:id2) %>%
+  write_csv("output/table1-model-id-mapping.csv")
+
+# And the first and second train/test indices just to make sure they don't change
+# THESE SHOULD NOT CHANGE
+full_model_grid %>%
+  pull(train_idx) %>%
+  `[[`(1) %>%
+  as.character() %>%
+  writeLines("output/table1-full-model-grid-train-idx-1.txt")
+full_model_grid %>%
+  pull(train_idx) %>%
+  `[[`(2) %>%
+  as.character() %>%
+  writeLines("output/table1-full-model-grid-train-idx-2.txt")
+
+# Save the full model grid as well, just in case.
+write_rds(full_model_grid, "output/table1-full-model-grid.rds")
+
+
+
+# Excecute the model runs -------------------------------------------------
+
+# For now I'm only interested in running the base specification row, so filter
+# on that. The average model also requires special treatment, so take it out.
+model_grid <- full_model_grid %>%
+  filter(table1_rows=="Base specification",
+         table1_columns!="Average",
+         table1_columns!="CAMEO",
+         hp_set!="Tuned",
+         table1_columns=="Escalation")
+
+dir.create("output/table1-chunks/prediction", showWarnings = FALSE, recursive = TRUE)
+dir.create("output/table1-chunks/roc", showWarnings = FALSE)
+
+# Take out models that have already been run
+if (!RERUN) {
+  stop("not implemented yet")
+}
 
 set.seed(5235)
-folds <- vfold_cv(train_df, v = 2, repeats = 7*3)
-map_dbl(folds$splits, function(x) {dat = testing(x); sum(dat[[dv]]=="1")})
 
-res <- foreach(i = 1:nrow(folds)) %dopar% {
-  lgr$info("Iteration %s of %s", i, nrow(folds))
-  res_i <- list()
+res <- foreach(
+  i = 1:nrow(model_grid),
+  .export = c("model_grid", "df", "spec_list", "dv"),
+  .packages = c("randomForest", "tibble", "yardstick", "dplyr"),
+  .inorder = FALSE) %dopar% {
 
-  train_i <- training(folds$splits[[i]])
-  test_i  <- testing(folds$splits[[i]])
+    # keep track of run time
+    t0 <- proc.time()
 
-  #
-  #   Escalation spec
-  #
+    chunk_id <- model_grid$model_id[[i]]
 
-  # Base escalation model (cf. first row in Table 1)
-  fitted_base     <- rf_base(train_i, escalation)
-  test_preds_base <- tibble(
-    preds = predict.rf_base(fitted_base, test_i),
-    truth = test_i[, dv]
-  )
+    train_i <- df[model_grid$train_idx[[i]], ]
+    test_i  <- df[model_grid$test_idx[[i]], ]
 
-  res_i <- c(res_i, list(
-    tibble(i = i,
-           model = "base escalation",
-           AUC = roc_auc(test_preds_base, truth, preds)[[".estimate"]]
-    )))
+    dv      <- dv
+    x_names <- spec_list[[model_grid$table1_columns[[i]]]]
 
-  # RF with default settings, escalation features
-  fitted_default     <- rf_default(train_i, escalation)
-  test_preds_default <- tibble(
-    preds = predict.rf_default(fitted_default, test_i),
-    truth = test_i[, dv]
-  )
+    hp_set <- model_grid$hp_set[[i]]
 
-  res_i <- c(res_i, list(
-    tibble(i = i,
-           model = "default escalation",
-           AUC = roc_auc(test_preds_default, truth, preds)[[".estimate"]]
-    )))
+    # Run the correct RF model given the HP set
+    if (hp_set=="B&S") {
+      fitted_model <- rf_base(train_i, x_names)
+      test_preds   <- tibble(
+        preds = predict.rf_base(fitted_model, test_i),
+        truth = test_i[, dv]
+      )
+    } else if (hp_set=="Default") {
+      fitted_model <- rf_default(train_i, x_names)
+      test_preds   <- tibble(
+        preds = predict.rf_default(fitted_model, test_i),
+        truth = test_i[, dv]
+      )
+    } else {
+      fitted_model <- rf_tuned(train_i, x_names)
+      test_preds   <- tibble(
+        preds = predict.rf_tuned(fitted_model, test_i),
+        truth = test_i[, dv]
+      )
+    }
 
-  # RF with tuned settings, escalation features
-  fitted_tuned     <- rf_tuned(train_i, escalation)
-  test_preds_tuned <- tibble(
-    preds = predict.rf_tuned(fitted_tuned, test_i),
-    truth = test_i[, dv]
-  )
+    # Write output
+    # 1. Predictions
+    pred_path <- file.path(sprintf("output/table1-chunks/prediction/chunk-%04d.rds", chunk_id))
+    write_rds(test_preds, path =pred_path)
 
-  res_i <- c(res_i, list(
-    tibble(i = i,
-           model = "tuned escalation",
-           AUC = roc_auc(test_preds_tuned, truth, preds)[[".estimate"]]
-    )))
+    # 2. Summary stats
+    res <- tibble(
+      model_id = chunk_id,
+      AUC = roc_auc(test_preds, truth, preds)[[".estimate"]]
+    )
+    roc_path <- file.path(sprintf("output/table1-chunks/roc/chunk-%04d.rds", chunk_id))
+    write_csv(res, roc_path)
 
-  #
-  #   Quad spec
-  #
-
-  # Base quad model (cf. first row in Table 1)
-  fitted_base     <- rf_base(train_i, quad)
-  test_preds_base <- tibble(
-    preds = predict.rf_base(fitted_base, test_i),
-    truth = test_i[, dv]
-  )
-
-  res_i <- c(res_i, list(
-    tibble(i = i,
-           model = "base quad",
-           AUC = roc_auc(test_preds_base, truth, preds)[[".estimate"]]
-    )))
-
-  # RF with default settings, quad features
-  fitted_default     <- rf_default(train_i, quad)
-  test_preds_default <- tibble(
-    preds = predict.rf_default(fitted_default, test_i),
-    truth = test_i[, dv]
-  )
-
-  res_i <- c(res_i, list(
-    tibble(i = i,
-           model = "default quad",
-           AUC = roc_auc(test_preds_default, truth, preds)[[".estimate"]]
-    )))
-
-  # RF with tuned settings, quad features
-  fitted_tuned     <- rf_tuned(train_i, quad)
-  test_preds_tuned <- tibble(
-    preds = predict.rf_tuned(fitted_tuned, test_i),
-    truth = test_i[, dv]
-  )
-
-  res_i <- c(res_i, list(
-    tibble(i = i,
-           model = "tuned quad",
-           AUC = roc_auc(test_preds_tuned, truth, preds)[[".estimate"]]
-    )))
-
-  #
-  #   Pull together results
-  #
-  res_i <- bind_rows(res_i)
-  res_i
-}
-
-res <- bind_rows(res)
-write_rds(res, "output/train-cv-resamples.rds")
-
-boot_ci <- function(x) {
-  bb <- boot(x, function(x, indices) mean(x[indices]), R = 2000)
-  ci <- boot.ci(bb, type = "basic")
-  out <- as.list(ci$basic[4:5])
-  names(out) <- c("ci_lower", "ci_upper")
-  as_tibble(out)
-}
-
-cv_fit <- res %>%
-  group_by(model) %>%
-  summarize(mean_AUC = mean(AUC),
-            sd_AUC   = sd(AUC),
-            ci = list(boot_ci(AUC))) %>%
-  unnest(ci)
-
-tbl <- cv_fit %>%
-  mutate(Test_ROC_AUC = rep(NA, nrow(cv_fit)))
-
-
-#
-#   Run models on full training data and get test fit ----
-#   _________________________________________________
-
-# Estimate the final models on the full training data
-
-#
-#   Escalation spec
-#
-
-# Base escalation model (cf. first row in Table 1)
-fitted_base     <- rf_base(train_df, escalation)
-test_preds_base <- tibble(
-  preds = predict.rf_base(fitted_base, test_df),
-  truth = test_df[, dv]
-)
-
-tbl$Test_ROC_AUC[tbl$model=="base escalation"] <- roc_auc(test_preds_base, truth, preds)[[".estimate"]]
-
-# Default RF escalation model
-fitted_default     <- rf_default(train_df, escalation)
-test_preds_default <- tibble(
-  preds = predict.rf_default(fitted_default, test_df),
-  truth = test_df[, dv]
-)
-
-tbl$Test_ROC_AUC[tbl$model=="default escalation"] <- roc_auc(test_preds_default, truth, preds)[[".estimate"]]
-
-# Tuned RF escalation model
-fitted_tuned     <- rf_tuned(train_df, escalation)
-test_preds_tuned <- tibble(
-  preds = predict.rf_tuned(fitted_tuned, test_df),
-  truth = test_df[, dv]
-)
-
-tbl$Test_ROC_AUC[tbl$model=="tuned escalation"] <- roc_auc(test_preds_tuned, truth, preds)[[".estimate"]]
-
-#
-#   Quad spec
-#
-
-# Base quad model (cf. first row in Table 1)
-fitted_base     <- rf_base(train_df, quad)
-test_preds_base <- tibble(
-  preds = predict.rf_base(fitted_base, test_df),
-  truth = test_df[, dv]
-)
-
-tbl$Test_ROC_AUC[tbl$model=="base quad"] <- roc_auc(test_preds_base, truth, preds)[[".estimate"]]
-
-# Default RF quad model
-fitted_default     <- rf_default(train_df, quad)
-test_preds_default <- tibble(
-  preds = predict.rf_default(fitted_default, test_df),
-  truth = test_df[, dv]
-)
-
-tbl$Test_ROC_AUC[tbl$model=="default quad"] <- roc_auc(test_preds_default, truth, preds)[[".estimate"]]
-
-# Tuned RF quad model
-fitted_tuned     <- rf_tuned(train_df, quad)
-test_preds_tuned <- tibble(
-  preds = predict.rf_tuned(fitted_tuned, test_df),
-  truth = test_df[, dv]
-)
-
-tbl$Test_ROC_AUC[tbl$model=="tuned quad"] <- roc_auc(test_preds_tuned, truth, preds)[[".estimate"]]
-
-# mdl_escalation_3 <- suppressWarnings({
-#   randomForest(y = as.integer(train_df$incidence_civil_ns_plus1=="1"),
-#                x = train_df[, all_cameo],
-#                type = "regression",
-#                ntree = 100000,
-#                maxnodes = 5,
-#                sampsize = 100,
-#                replace = FALSE,
-#                do.trace = FALSE)
-# })
-#
-# test_preds_3 <- tibble(
-#   preds = as.vector(predict(mdl_escalation_3, newdata = test_df[, all_cameo],
-#                             type = "response")),
-#   truth = test_df[, dv])
-# tbl$Test_ROC_AUC[3] <- roc_auc(test_preds_3, truth, preds)[[".estimate"]]
-#
-#
-#
-# mdl_escalation_4 <- randomForest(y = train_df$incidence_civil_ns_plus1,
-#                                  x = train_df[, all_cameo],
-#                                  type = "classification",
-#                                  ntree = 5000,
-#                                  mtry  = 3,
-#                                  replace = TRUE,
-#                                  do.trace = FALSE)
-#
-# test_preds_4 <- tibble(
-#   preds = as.vector(predict(mdl_escalation_4, newdata = test_df[, escalation], type = "prob")[, "1"]),
-#   truth = test_df[, dv])
-# tbl$Test_ROC_AUC[4] <- roc_auc(test_preds_4, truth, preds)[[".estimate"]]
-
-write_rds(tbl, "output/test-auc-roc.rds")
-tbl %>%
-  mutate(spec = stringr::str_extract(model, "[a-z]+$")) %>%
-  arrange(spec, model) %>%
-  select(-spec) %>%
-  knitr::kable(digits = 2) %>%
-  writeLines("output/tbl-traincv-test-fit.md")
-
-tbl
-
-
-# Difference of means test. Are the base and modified model AUC scores in the
-# train CV different?
-t.test(AUC ~ model, data = res[res$model %in% c("base escalation", "default escalation"), ])
-# yep, p < 0.05 thus model 1 performs better than model 2.
-
-t.test(AUC ~ model, data = res[res$model %in% c("base escalation", "tuned escalation"), ])
-
-# Roc curve for first escalation baseline model
-roc_escalation <- roc_curve(test_preds_base, truth, preds)
-autoplot(roc_escalation)
-
-# the roc curve has a big kink because a bunch of cases have identical prediction
-test_preds_base %>% count(preds) %>% arrange(desc(n))
-
-
-
-
-
+    NULL
+  }
