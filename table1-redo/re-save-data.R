7686dd8a811550246c106fcbd14f3d6ffadfdf64
Andreas Beger <adbeger@gmail.com>
2020-05-06 17:29:40 +0300

    start updating code for 6-month data

--- table1-redo/re-save-data.R
+++ table1-redo/re-save-data.R
@@ -1,5 +1,5 @@
 #
-#   Re-save the replication data as RDS, the .dta versions take very long to
+#   Re-save the replication data as RDS; the .dta versions take very long to
 #   read.
 #
 
@@ -7,8 +7,10 @@ library(readstata13)
 library(readr)
 library(here)
 
-setwd(here::here("tuning-experiments"))
+setwd(here::here("table1-redo"))
 
 data_1mo <- read.dta13("input-data/1mo_data.dta")
-
 write_rds(data_1mo, "trafo-data/1mo_data.rds")
+
+data_6mo <- read.dta13("input-data/6mo_data.dta")
+write_rds(data_1mo, "trafo-data/6mo_data.rds")
