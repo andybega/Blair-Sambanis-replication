35193c433cc653d6e64dee2b70a6605315ef7f5c
Andreas Beger <adbeger@gmail.com>
2020-06-01 12:52:37 +0300

    re-run RF models

--- rep_nosmooth/recreate-tables.R
+++ rep_nosmooth/recreate-tables.R
@@ -1,4 +1,6 @@
 
+library(readr)
+library(dplyr)
 library(tidyr)
 library(kableExtra)
 
@@ -24,4 +26,49 @@ table1_nosmooth <- results %>%
 
 write_csv(table1_nosmooth, "output/table1-nosmooth.csv")
 
+smooth_benefit <- results %>%
+  filter(table=="Table 1") %>%
+  mutate(diff = auc_roc_smoothed - auc_roc) %>%
+  select(horizon, row, column, diff) %>%
+  arrange(horizon, row) %>%
+  pivot_wider(names_from = "column", values_from = "diff") %>%
+  select(horizon, row, Escalation, Quad, Goldstein, CAMEO, Average) %>%
+  rename(Model = row)
 
+write_csv(smooth_benefit, "output/table1-smooth-benefit.csv")
+
+# N_train
+
+results %>%
+  filter(table=="Table 1") %>%
+  select(table, horizon, row, column, N_train) %>%
+  arrange(table, horizon, row) %>%
+  pivot_wider(names_from = "column", values_from = "N_train") %>%
+  select(table, horizon, row, Escalation, Quad, Goldstein, CAMEO, Average) %>%
+  rename(Model = row)
+
+results %>%
+  filter(table=="Table 2") %>%
+  select(table, horizon, row, column, N_train) %>%
+  arrange(table, horizon, row) %>%
+  pivot_wider(names_from = "column", values_from = "N_train") %>%
+  select(table, horizon, row, everything()) %>%
+  rename(Model = row)
+
+# N_test
+
+results %>%
+  filter(table=="Table 1") %>%
+  select(table, horizon, row, column, N_test) %>%
+  arrange(table, horizon, row) %>%
+  pivot_wider(names_from = "column", values_from = "N_test") %>%
+  select(table, horizon, row, Escalation, Quad, Goldstein, CAMEO, Average) %>%
+  rename(Model = row)
+
+results %>%
+  filter(table=="Table 2") %>%
+  select(table, horizon, row, column, N_test) %>%
+  arrange(table, horizon, row) %>%
+  pivot_wider(names_from = "column", values_from = "N_test") %>%
+  select(table, horizon, row, everything()) %>%
+  rename(Model = row)
