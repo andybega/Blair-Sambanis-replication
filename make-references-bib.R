c0fa3c35b104eb3ab9e7ba3c8c2d6c61933d8d93
Andreas Beger <adbeger@gmail.com>
2020-06-10 12:41:14 +0300

    add missing refs

--- make-references-bib.R
+++ make-references-bib.R
@@ -2,14 +2,12 @@
 #   Create the local references.bib file from the private central bib file
 #
 
-library(stringr)
-library(RefManageR)
-
 setwd(here::here("./"))
 
-source("https://raw.githubusercontent.com/andybega/condensebib/master/R/reduce-bib.R")
+remotes::install_github("andybega/condensebib")
+library(condensebib)
 
-reduce_bib(
+res <- reduce_bib(
   file = "paper.Rmd",
   master_bib = "../../whistle/master.bib",
   out_bib    = "references.bib"
@@ -17,5 +15,15 @@ reduce_bib(
 
 # Change \aa to \r{a}
 bib <- readLines("references.bib")
-bib <- str_replace_all(bib, "\\\\aa", "\\\\r\\{a\\}")
+bib <- stringr::str_replace_all(bib, "\\\\aa", "\\\\r\\{a\\}")
 writeLines(bib, "references.bib")
+
+
+# If some references are not showing, check if RefManageR complains about them
+if (FALSE) {
+  msgs <- capture.output(bib <- read_bib("../../whistle/master.bib", quiet = FALSE),
+                         type = "message")
+}
+
+
+
