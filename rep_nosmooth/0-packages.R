a2f63b21055218e3907b9e0d49f4bb0d2bf10dd1
Andreas Beger <adbeger@gmail.com>
2020-11-09 15:34:47 +0200

    update required packages

--- rep_nosmooth/0-packages.R
+++ rep_nosmooth/0-packages.R
@@ -5,15 +5,16 @@
 check_packages <- function() {
   installed <- rownames(installed.packages())
 
-  need <- c("dplyr", "readr", "randomForest", "yaml", "jsonlite", "pROC",
-            "foreach", "doFuture", "doRNG", "lgr", "tidyr", "ggplot2", "stringr",
-            "rsample", "readstata13", "here")
+  need <- c("doFuture", "doRNG", "dplyr", "forcats", "foreach", "ggplot2",
+            "here", "jsonlite", "kableExtra", "knitr", "lgr", "pROC", "purrr",
+            "randomForest", "readr", "readstata13", "rsample", "stringr",
+            "tibble", "tidyr", "tidyverse", "yaml", "yardstick")
 
   not_installed <- need[!need %in% installed]
 
   if ("dplyr" %in% installed) {
     # need v 1.0.0 or greater in order to be able to group_by a list column in
-    # recreate-tables.R
+    # 3-recreate-tables.R
     vv <- packageVersion("dplyr")
     if (vv < "1.0.0") {
       message(sprintf("dplyr >= 1.0.0 is required; found %s; adding to install list", vv))
